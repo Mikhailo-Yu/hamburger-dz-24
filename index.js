6f357853b3dd32f3a149165b24d6aa58ac2d0d80
Mykhailo-YU <miha.jewish@gmail.com>
2023-04-13 12:50:06 +0300

    init

--- index.js
+++ index.js
@@ -1,61 +1,53 @@
 const HamburgerSize = {
-    SMALL: { price: 50, calories: 20 },
-    LARGE: { price: 100, calories: 40 },
-  };
-  
-  const HamburgerStuffing = {
-    CHEESE: { price: 10, calories: 20 },
-    SALAD: { price: 20, calories: 5 },
-    POTATO: { price: 15, calories: 10 },
-  };
+  SMAL: {price: 50, calories: 20},
+  LARGE: {price: 100, calories: 50} 
+}
 
-  class Hamburger {
-    constructor(size, stuffing) {
-      this.size = size;
-      this.stuffing = stuffing;
-      this.toppings = [];
-    }
-  
-    addTopping(topping) {
-      this.toppings.push(topping);
-    }
-  
-    calculatePrice() {
-      let price = this.size.price + this.stuffing.price;
-      for (let topping of this.toppings) {
-        price += topping.price;
-      }
-      return price;
-    }
-  
-    calculateCalories() {
-      let calories = this.size.calories + this.stuffing.calories;
-      for (let topping of this.toppings) {
-        calories += topping.calories;
-      }
-      return calories;
-    }
+const HamburgerStuffing = {
+  CHEESE: {price: 10, calories: 20},
+  SALAD: {price: 20, calories: 5},
+  POTATO: {price: 15, calories: 10}
+} 
+const HamburgerTopping = {
+  MAYO: {price: 20, calories: 5},
+  SPICE: {price: 15, calories: 0} 
+}
+
+class Hamburger  {
+  constructor (size,  stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.topping = [];
   }
 
-  const HamburgerTopping = {
-    SPICE: { price: 15, calories: 0 },
-    MAYO: { price: 20, calories: 5 },
-  };
+  addTopping (topping) {
+    this.topping.push(topping); 
+  }
+  calculatePrice() {
+   return this.size.price + this.stuffing.price + this.topping.reduce((acc, topping) => acc + topping.price, 0);
+  }
+  calculateCalories() {
+   return this.size.calories + this.stuffing.calories + this.topping.reduce((acc, topping) => acc + topping.calories, 0);
+  }
+}
 
-  // створюємо маленький гамбургер з начинкою з сиру
-const hamburger = new Hamburger(HamburgerSize.SMALL, HamburgerStuffing.CHEESE);
+// маленький гамбурег
+const hamburger = new Hamburger (HamburgerSize.SMAL, HamburgerStuffing.CHEESE);
 
-// додаємо добавку з майонезу
+//додаємо майонез
 hamburger.addTopping(HamburgerTopping.MAYO);
 
-// виводимо калорійність
-console.log("Calories: " + hamburger.calculateCalories());
+// Дізнаємось скільки калорій
+console.log('Calories: '  + hamburger.calculateCalories());
 
-// виводимо ціну
+// Дізнаємось скільки коштує
+
 console.log("Price: " + hamburger.calculatePrice());
 
-// додаємо приправу
+// я тут передумав і вирішив додати ще приправу
 hamburger.addTopping(HamburgerTopping.SPICE);
 
-// виводимо ціну з приправою
+// А скільки тепер коштує?
 console.log("Price with sauce: " + hamburger.calculatePrice());
+// Дізнаємось скільки калорій
+console.log('Calories: '  + hamburger.calculateCalories());
